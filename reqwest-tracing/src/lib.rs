753b2439fafa64e9fd94e5a4165ca027c0c9b497
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2021-08-30 14:14:06 +0300

    Add support for OpenTelemetry 0.16 and tracing-opentelemetry 0.15 (#2)
    * add opentelemetry 0.16
    
    * ci: run against otel 0.16
    
    * ci: run jobs against each otel version
    
    * docs: mention opentelemetry_0_16 in README

--- reqwest-tracing/src/lib.rs
+++ reqwest-tracing/src/lib.rs
@@ -2,7 +2,8 @@ mod middleware;
 #[cfg(any(
     feature = "opentelemetry_0_13",
     feature = "opentelemetry_0_14",
-    feature = "opentelemetry_0_15"
+    feature = "opentelemetry_0_15",
+    feature = "opentelemetry_0_16",
 ))]
 mod otel;
 
