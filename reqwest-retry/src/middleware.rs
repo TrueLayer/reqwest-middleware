9fd697e8b30f742d9ab90847421566f11edb3671
Rutger <rutgerdj2@gmail.com>
2022-02-23 19:05:36 +0100

    Add constructor to create a `RetryTransientMiddleware` with a custom `RetryableStrategy`

--- reqwest-retry/src/middleware.rs
+++ reqwest-retry/src/middleware.rs
@@ -49,6 +49,17 @@ impl<T: RetryPolicy + Send + Sync> RetryTransientMiddleware<T> {
             retryable_strategy: RetryableStrategy::default(),
         }
     }
+
+    /// Construct a [`RetryTransientMiddleware`] With a given [`RetryPolicy`] and [`RetryableStrategy`]
+    pub fn new_with_retryable_strat(
+        retry_policy: T, 
+        retryable_strategy: RetryableStrategy
+    ) -> Self {
+        Self {
+            retry_policy,
+            retryable_strategy
+        }
+    }
 }
 
 #[async_trait::async_trait]
