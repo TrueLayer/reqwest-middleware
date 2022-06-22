90d81c67a65b2890208953d8a7bb25c320143bc7
azanin <ale.zanin90@gmail.com>
2022-06-22 19:37:29 +0200

    Fix empty line

--- reqwest-retry/tests/all/helpers/simple_server.rs
+++ reqwest-retry/tests/all/helpers/simple_server.rs
@@ -4,6 +4,7 @@ use async_std::net::{TcpListener, TcpStream};
 use futures::stream::StreamExt;
 use std::error::Error;
 use std::fmt;
+
 /// This is a simple server that returns the responses given at creation time: [`self.raw_http_responses`] following a round-robin mechanism.
 pub struct SimpleServer {
     listener: TcpListener,
