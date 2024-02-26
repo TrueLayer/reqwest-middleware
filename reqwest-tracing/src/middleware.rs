9c866ad0cf66d77dc46a8841c749b0bfbe79ae15
Robert Collins <robert.collins@cognite.com>
2024-02-26 19:32:54 +0000

    Support opentelemetry 0.22

--- reqwest-tracing/src/middleware.rs
+++ reqwest-tracing/src/middleware.rs
@@ -55,6 +55,7 @@ where
                 feature = "opentelemetry_0_19",
                 feature = "opentelemetry_0_20",
                 feature = "opentelemetry_0_21",
+                feature = "opentelemetry_0_22",
             ))]
             let req = if !extensions.contains::<crate::DisableOtelPropagation>() {
                 // Adds tracing headers to the given request to propagate the OpenTelemetry context to downstream revivers of the request.
